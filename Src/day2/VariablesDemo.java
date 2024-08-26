b4ed5f8a76775ce578df8cb1e1975be0b155cceb
Grisel <fabiana.gonzalez@frsc.utn.edu.ar>
2024-08-26 10:05:09 -0300

    Change in VariablesDemo.java

---
+++ Src/day2/VariablesDemo.java
@@ -0,0 +1,42 @@
+package day2;
+
+public class VariablesDemo {
+
+    public static void main(String[] args) {
+
+        /*
+         int a; //declaration
+         a =100; // assignment
+        */
+
+        /*
+        int a = 100; //declaration+assignment
+        System.out.println(a);//100
+        a= 200;
+        System.out.println(a);
+        */
+
+        //Approach1 - if all the variables belongs to dif data types
+        /*
+        int a =100;
+        int b = 200;
+        int c = 230;
+         */
+
+        //Approach2 - if all the variables belong to same data type
+        /*
+        int a,b,c;
+        a=100;
+        b=200;
+        c=250,
+        */
+        
+        //Approach3 - if all the variables are belong to the same data type
+        /*
+        int a= 100,b=200,c=300;
+        System.out.println("The value of a is " + a);
+        System.out.println("The value of b is " + b);
+        System.out.println("The value of c is " + c);
+        */
+    }
+}
