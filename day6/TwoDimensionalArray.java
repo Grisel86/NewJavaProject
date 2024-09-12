f3cd256047a8fe0376e6aef3895d04a8877c7fe0
Grisel <fabiana.gonzalez@frsc.utn.edu.ar>
2024-09-12 19:42:07 -0300

    Single and Two dimensional arrays

---
+++ day6/TwoDimensionalArray.java
@@ -0,0 +1,19 @@
+package day6;
+
+public class TwoDimensionalArray {
+    public static void main(String[] args) {
+
+        //declaring array
+        //Aproach1
+        int[][] a = new int[3][2];
+
+        a[0][0] = 100;
+        a[0][1] = 200;
+
+        a[1][0] = 300;
+        a[1][1] = 400;
+
+        a[2][0] = 500;
+        a[2][1] = 600;
+    }
+}
