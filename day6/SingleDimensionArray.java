3318eb7db1cbc1e337266970c6b223cea265c15f
Grisel <fabiana.gonzalez@frsc.utn.edu.ar>
2024-09-22 17:11:07 -0300

    Single dimension updated

---
+++ day6/SingleDimensionArray.java
@@ -0,0 +1,34 @@
+package day6;
+
+public class SingleDimensionArray {
+    public static void main(String[] args)
+    {
+        //declaring array
+        //Approach1
+        /*int a[]=new int[5];
+        a[0]=100;
+        a[1]=200;
+        a[2]=300;
+        a[3]=400;
+        a[4]=500;
+         */
+
+        //Approach2
+        int a[] = {100,200,300,400,500};
+
+        //find length of an array
+        System.out.println("Length of an array is: " + a.length);
+
+        //read single value from an array
+        //System.out.println(a[4]); //here 4 is index
+
+        //reading all the values from array
+
+        //Normal for loop - to know how the quantity of numbers that I have
+        for (int i=0;i<a.length;i++)  // i<=4  i<5  i<a.length-1  i<a.length
+        {
+            System.out.println(a[i]);
+        }
+
+    }
+}
